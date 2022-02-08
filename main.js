dfb0bd6d94ef6fff5b500e70980769c71aec7ba4
bluuweb <contacto@bluuweb.cl>
2022-02-08 12:59:25 -0300

    agregamos el promedio

--- main.js
+++ main.js
@@ -14,6 +14,13 @@ class Estudiante extends Persona {
     set agregarNota(value) {
         this.notas.push(value);
     }
+
+    sacarPromedio() {
+        const suma = this.notas.reduce((acc, current) => acc + current, 0);
+
+        console.log(suma);
+        return (suma / this.notas.length).toFixed(1);
+    }
 }
 
 const formulario = document.querySelector("#formulario");
@@ -32,19 +39,22 @@ formulario.addEventListener("submit", (evento) => {
 
     // instancia del objeto
     const estudianteUno = new Estudiante(inputNombre.value, inputEdad.value);
-    estudianteUno.agregarNota = inputNota1.value;
-    estudianteUno.agregarNota = inputNota2.value;
-    estudianteUno.agregarNota = inputNota3.value;
+    estudianteUno.agregarNota = +inputNota1.value;
+    estudianteUno.agregarNota = +inputNota2.value;
+    estudianteUno.agregarNota = +inputNota3.value;
 
     console.log(estudianteUno);
 
     resultado.innerHTML = `
-    <p>El estudiante con nombre: ${estudianteUno.nombre}, tiene una edad de: ${estudianteUno.edad}</p>
+    <p>El estudiante con nombre: ${estudianteUno.nombre}, tiene una edad de: ${
+        estudianteUno.edad
+    }</p>
     <p>Sus notas son:</p>
     <ul>
         <li>Nota1: ${estudianteUno.notas[0]}</li>
         <li>Nota2: ${estudianteUno.notas[1]}</li>
         <li>Nota3: ${estudianteUno.notas[2]}</li>
     </ul>
+    <p>Su promedio final es: ${estudianteUno.sacarPromedio()}</p>
     `;
 });
